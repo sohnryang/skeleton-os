58103aff6876e512ba64baa37a3edd465fa29468
Ryang Sohn <loop.infinitely@gmail.com>
2021-09-17 11:58:59 +0900

    Add comments to header file

--- include/console/vga_terminal.h
+++ include/console/vga_terminal.h
@@ -1,9 +1,13 @@
+/*
+ * vga_terminal.h -- VGA terminal output functions
+ */
+
 #include <stddef.h>
 #include <stdint.h>
 
 #pragma once
 
-enum vga_color {
+enum vga_color {  // VGA color codes
   VGA_COLOR_BLACK = 0,
   VGA_COLOR_BLUE,
   VGA_COLOR_GREEN,
@@ -22,15 +26,26 @@ enum vga_color {
   VGA_COLOR_WHITE
 };
 
-extern size_t terminal_row, terminal_col;
-extern uint8_t terminal_color;
-extern uint16_t *terminal_buf;
+extern size_t terminal_row, terminal_col;  // current locatio of cursor
+extern uint8_t terminal_color;  // current background, foreground color of texts
+                                // being printed
+extern uint16_t *terminal_buf;  // terminal buffer.
 
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 25;
+static const size_t VGA_WIDTH = 80;   // VGA terminal width
+static const size_t VGA_HEIGHT = 25;  // VGA terminal height
 
+// Initializes the terminal output by setting terminal_* variables
 void init_terminal();
+
+// Sets color of texts being printed
 void term_set_color(uint8_t color);
+
+// Puts an entry to VGA terminal. An entry consists of char, color code, and
+// location
 void term_put_entry(char ch, uint8_t color, size_t x, size_t y);
+
+// Puts a character to current cursor location, in colors set by term_set_color
 void term_put_char(char ch);
+
+// Puts a string to VGA terminal
 void term_put_str(const char *data);
