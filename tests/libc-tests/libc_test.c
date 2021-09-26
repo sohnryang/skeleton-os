0a6ae931a40558eec21e21603682d58cb6c2fcb5
Ryang Sohn <loop.infinitely@gmail.com>
2021-09-27 08:19:18 +0900

    Add `__attribute__((unused))` to suppress warning

--- tests/libc-tests/libc_test.c
+++ tests/libc-tests/libc_test.c
@@ -9,19 +9,19 @@
 #include <cmocka.h>
 // clang-format on
 
-static void test_kstrlen(void **state) {
+static void test_kstrlen(__attribute__((unused)) void **state) {
   assert_int_equal(kstrlen("hell"), 4);
   assert_int_equal(kstrlen("skeletonOS"), 10);
 }
 
-static void test_kstrcpy(void **state) {
+static void test_kstrcpy(__attribute__((unused)) void **state) {
   char str1[5] = "test";
   char str2[5];
   kstrcpy(str2, str1);
   assert_string_equal(str2, "test");
 }
 
-static void test_kstrncpy(void **state) {
+static void test_kstrncpy(__attribute__((unused)) void **state) {
   char str1[11] = "skeletonOS";
   char str2[9];
   kstrncpy(str2, str1, 8);
