47b283627d373bd906d489ede9fb5a710d7bacef
Ryang Sohn <loop.infinitely@gmail.com>
2021-09-27 00:35:17 +0900

    Add a test for `kstrcpy`

--- tests/libc-tests/libc_test.c
+++ tests/libc-tests/libc_test.c
@@ -14,7 +14,15 @@ static void test_kstrlen(void **state) {
   assert_int_equal(kstrlen("skeletonOS"), 10);
 }
 
+static void test_kstrcpy(void **state) {
+  char str1[5] = "test";
+  char str2[5];
+  kstrcpy(str2, str1);
+  assert_string_equal(str2, "test");
+}
+
 int main() {
-  const struct CMUnitTest tests[] = {cmocka_unit_test(test_kstrlen)};
+  const struct CMUnitTest tests[] = {cmocka_unit_test(test_kstrlen),
+                                     cmocka_unit_test(test_kstrcpy)};
   return cmocka_run_group_tests(tests, NULL, NULL);
 }
