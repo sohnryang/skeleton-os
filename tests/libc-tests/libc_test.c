b8e5917692e72efbfdfb08f3aa00e596cec612cc
Ryang Sohn <loop.infinitely@gmail.com>
2021-09-27 00:34:30 +0900

    Rename test to match function name

--- tests/libc-tests/libc_test.c
+++ tests/libc-tests/libc_test.c
@@ -9,12 +9,12 @@
 #include <cmocka.h>
 // clang-format on
 
-static void test_strlen(void **state) {
+static void test_kstrlen(void **state) {
   assert_int_equal(kstrlen("hell"), 4);
   assert_int_equal(kstrlen("skeletonOS"), 10);
 }
 
 int main() {
-  const struct CMUnitTest tests[] = {cmocka_unit_test(test_strlen)};
+  const struct CMUnitTest tests[] = {cmocka_unit_test(test_kstrlen)};
   return cmocka_run_group_tests(tests, NULL, NULL);
 }
