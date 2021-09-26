0bc7b27a68057e3949db1a4abc1d3b7aff3c31e0
Ryang Sohn <loop.infinitely@gmail.com>
2021-09-27 08:12:56 +0900

    Add `kstrncpy`, a `strncpy` equivalent

--- include/libc/string.h
+++ include/libc/string.h
@@ -8,4 +8,6 @@
 
 size_t kstrlen(const char* s);        // Calculates the length of given string.
 void kstrcpy(char* dest, char* src);  // Copies string in src to dest
-void kitoa(int x, char* out);         // Converts int to string.
+void kstrncpy(char* dest, char* src,
+              size_t n);       // Copies n characters from string in src to dest
+void kitoa(int x, char* out);  // Converts int to string.
--- src/libc/string.c
+++ src/libc/string.c
@@ -10,3 +10,9 @@ size_t kstrlen(const char* s) {
 void kstrcpy(char* dest, char* src) {
   for (size_t i = 0; src[i]; ++i) dest[i] = src[i];
 }
+
+void kstrncpy(char* dest, char* src, size_t n) {
+  size_t i;
+  for (i = 0; i < n && src[i] != '\0'; ++i) dest[i] = src[i];
+  for (; i < n; ++i) dest[i] = '\0';
+}
