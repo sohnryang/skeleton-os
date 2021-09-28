9505c8da34557e050233961845fff9904104ec38
Ryang Sohn <loop.infinitely@gmail.com>
2021-09-29 08:02:10 +0900

    Add `kmemmove`, a `memmove` equivalent

--- include/libc/string.h
+++ include/libc/string.h
@@ -12,3 +12,6 @@ void kstrncpy(char* dest, char* src,
               size_t n);       // Copies n characters from string in src to dest
 void kitoa(int x, char* out);  // Converts int to string.
 void kitoa_hex(int x, char* out);  // Converts int to base-16 string.
+void kmemmove(
+    void* dest, const void* src,
+    size_t n);  // Copies n bytes from src to dest. Memory areas can overlap.
--- src/libc/string.c
+++ src/libc/string.c
@@ -52,3 +52,15 @@ void kitoa_hex(int x, char* out) {
   if (current == 0) result[current++] = '0';
   kstrncpy(out, result, current + 1);
 }
+
+void kmemmove(void* dest, const void* src, size_t n) {
+  char* d = dest;
+  const char* s = src;
+
+  if (d == s) return;
+
+  if (d < s)
+    for (; n; n--) *d++ = *s++;
+  else
+    while (n) n--, d[n] = s[n];
+}
