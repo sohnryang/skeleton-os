98e9305467ef22a4f0291a3e66f98ea1cfa19db6
Ryang Sohn <loop.infinitely@gmail.com>
2021-10-10 08:50:34 +0900

    Delete various kprintf testing code

--- src/arch/i686/kernel_main.c
+++ src/arch/i686/kernel_main.c
@@ -7,9 +7,4 @@
 void kernel_main() {
   init_terminal();
   kprintf(INFO, "Welcome to skeletonOS %s!\n", "0.0.0");
-  kprintf(DEBUG, "Hell world\n");
-  kprintf(DEBUG, "%d %x %c\n", 42, 0xdeadbeef, '?');
-  int x = 0;
-  int* ptr = &x;
-  kprintf(DEBUG, "Pointer to x: %p\n", ptr);
 }
