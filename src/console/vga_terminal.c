c161cdc6e27ff14a03729413d3c5b03bcebad8e2
Ryang Sohn <loop.infinitely@gmail.com>
2021-12-08 18:54:14 +0900

    Add a comment about portability

--- src/console/vga_terminal.c
+++ src/console/vga_terminal.c
@@ -20,7 +20,8 @@ void init_terminal() {
   terminal_row = 0;
   terminal_col = 0;
   terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GRAY, VGA_COLOR_BLACK);
-  terminal_buf = (uint16_t *)0xb8000;
+  terminal_buf = (uint16_t *)0xb8000;  // FIXME: framebuffer address can differ
+                                       // between platforms
   for (size_t y = 0; y < VGA_HEIGHT; ++y)
     for (size_t x = 0; x < VGA_WIDTH; ++x)
       terminal_buf[pos_to_index(x, y)] = vga_entry(' ', terminal_color);
